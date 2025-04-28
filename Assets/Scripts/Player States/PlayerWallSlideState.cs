54a6915ea353671f18d8c1d80436b35629b9feae
luminostar <rafialayyubi08@gmail.com>
2025-04-28 15:15:28 +0800

    edit : fix wall slide

--- Assets/Scripts/Player States/PlayerWallSlideState.cs
+++ Assets/Scripts/Player States/PlayerWallSlideState.cs
@@ -16,15 +16,23 @@ public class PlayerWallSlideState : PlayerBaseState
 
         manager.playerMovement.WallSlide();
 
+        if (manager.playerMovement.isGrounded())
+        {
+            manager.SwitchState(Mathf.Abs(moveInput) > 0.1f ? manager.moveState : manager.idleState);
+            return;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             manager.playerMovement.WallJump();
             manager.SwitchState(manager.jumpState);
+            return;
         }
 
-        if (!manager.playerMovement.IsTouchingWall() || manager.playerMovement.isGrounded())
+        if (!manager.playerMovement.IsTouchingWall())
         {
             manager.SwitchState(manager.fallState);
+            return;
         }
     }
 }
